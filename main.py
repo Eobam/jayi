2f5d6832e7aaab2c775be2598602cb9cdba0ac82
Eobam <elliottlieberman123@gmail.com>
2026-04-17 15:53:49 -0400

    ts cool but broke

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ if "hcb" in prompt.lower():
     print("hmm, i'll talk to alfie")
 
 
-elif "blueprint" in prompt.lower:
+elif "blueprint" in prompt.lower():
     print("debt... debt... DEBT")
+
+elif "jay dont" or "jay don't" in prompt.lower():
+    print("Leo taught me how to mute that")
+
+elif "unc" in prompt.lower():
+    print("i'm not dev sob")
 
 else:
     print(random.choice(random_responses))
