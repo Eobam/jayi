6738a3f33ce488beb3ac92ef1cc04eba6b957f62
Eobam <elliottlieberman123@gmail.com>
2026-04-17 12:30:03 -0400

    basic thingie works

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import random
+
 prompt = input("Talk to Jay")
 
+random_responses = [":neocat-floof-explode:", "yep", "ok got it", "oo", "hehe", "LMAO", "oop why?", "bruh" ]
 
 
 if "hcb" in prompt.lower():
     print("hmm, i'll talk to alfie")
 
 else:
-    pass
+    print(random.choice(random_responses))
+    
 
-random_responses = [":neocat-floof-explode:", "yep", "ok got it", "oo", "hehe", "LMAO", "oop why?", "bruh" ]
+
